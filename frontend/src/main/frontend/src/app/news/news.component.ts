a93acb43bea317921b21582e7c83c129143526fd
pelangov <poongodi.e.87@gmail.com>
2017-05-16 15:35:39 -0400

    Updated news component

--- frontend/src/main/frontend/src/app/news/news.component.ts
+++ frontend/src/main/frontend/src/app/news/news.component.ts
@@ -9,34 +9,45 @@ import {Component} from '@angular/core';
                 <td width="350px" style="border: 0; padding: 0;float: left;">
                     <table width="350px" height="250px" border="0">
                         <tr>
-                            <td style= "background-size: cover; background-repeat: no-repeat; vertical-align: bottom; padding: 0">
+                          <td style="background-image:url('../../images/dashboard_logo.png'); background-size: cover; background-repeat: no-repeat; vertical-align: bottom; padding: 0">
                                 <div class="widgetimg pink-gradient">
-                                    <a style="color: #FFFFFF !important;">                                    
-                                        <span style=" color: #FFFFFF; ">PRA NEWS</span>
-                                    </a>
+                                  <a href="#" style="color: #FFFFFF !important;">
+                                    <span style=" color: #FFFFFF; ">PRA NEWS</span>
+                                  </a>
                                 </div>
                             </td>
                         </tr>
                     </table> 
-                     <article style="width: 350px">                        
-                        <p class="date">
-                            Display Date
-                        </p>
-                        <p class="excerpt">News Teaser...</p><br />
-                        <a class="read-more" style="float: left;">Read Full Article</a>
-                        <span class="read-morearrow">→</span>
+                     <article style="width: 350px">
+                       <h2 class="title"><a href="#">Sample News</a></h2>
+                       <p class="date">MON, Apr 1, 2015
+                       </p>
+                       <p class="excerpt">Dynamic content to be loaded from DB</p><br />
+                       <a class="read-more" href="#" style="float: left;">Read Full Article</a>
+                       <span class="read-morearrow">&#8594;</span>
                     </article>  
                  </td>
                   <!-- PRA News Feed -->
                 <td style="border: 0; padding: 0; background: linear-gradient(to bottom, #f9f9f9, #dedcdc);filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f9f9f9', endColorstr='#dedcdc',GradientType=0 ); width: 100%">
-                    <ul class="pra-news-feed">                        
-                            <li>
-                                <h3 class="title">
-                                Repeat list
-                                </h3>
-                                <div class="date">                                     
-                                </div>
-                            </li>                       
+                    <ul class="pra-news-feed">                          
+                      <li>
+                        <h3 class="title"><a href="#">
+                          News 1</a></h3>
+                        <div class="date">THU, May 11, 2015
+                        </div>
+                      </li>
+                      <li>
+                        <h3 class="title"><a href="https://pra--c.na28.visual.force.com/apex/IPA_Article?Id=a7B1A000000GyhMUAS">
+                          News 2</a></h3>
+                        <div class="date">TUE, May 09, 2015
+                        </div>
+                      </li>
+                      <li>
+                        <h3 class="title"><a href="https://pra--c.na28.visual.force.com/apex/IPA_Article?Id=a7B1A000000GyVkUAK">
+                          News 3</a></h3>
+                        <div class="date">FRI, May 05, 2015
+                        </div>
+                      </li>                                                        
                     </ul>
                 </td>
             </tr>
